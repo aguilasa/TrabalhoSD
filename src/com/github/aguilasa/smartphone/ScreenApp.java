5fcd7f2c320bb1e8c712b3a3f13b3a9057032496
Ingmar Aguiar <ingmar.aguiar@senior.com.br>
2017-09-15 13:31:24 -0300

    Screen

--- src/com/github/aguilasa/smartphone/ScreenApp.java
+++ src/com/github/aguilasa/smartphone/ScreenApp.java
@@ -1,5 +1,20 @@
 package com.github.aguilasa.smartphone;
 
+import static javax.swing.JOptionPane.DEFAULT_OPTION;
+import static javax.swing.JOptionPane.WARNING_MESSAGE;
+import static javax.swing.JOptionPane.YES_OPTION;
+import static javax.swing.JOptionPane.showOptionDialog;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowEvent;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
@@ -8,44 +23,29 @@ import java.net.MulticastSocket;
 import java.net.NetworkInterface;
 import java.net.SocketAddress;
 
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.github.aguilasa.common.Chronometer;
 import com.github.aguilasa.common.Processing;
 
-public class ScreenApp extends BaseScreen {
+public class ScreenApp extends JFrame {
 
-	private static final long serialVersionUID = -6145320864740849476L;
+	private static final long serialVersionUID = 6315955425937210662L;
+
+	private JPanel moverPanel;
+	protected JPanel panel;
 
 	private MulticastSocket multicastSocket = null;
 	private SocketAddress socketAddress = null;
 	private ReceiveReader receiveReader;
 	private Thread receiveThread;
 	private Sender sender;
-
-	public ScreenApp() {
-		super();
-
-		socketAddress = new InetSocketAddress("228.5.6.7", 4321);
-
-		try {
-			multicastSocket = new MulticastSocket(4321);
-			multicastSocket.setLoopbackMode(false);
-
-			InetAddress localHost = InetAddress.getLocalHost();
-			NetworkInterface networkInterface = NetworkInterface.getByInetAddress(localHost);
-			multicastSocket.joinGroup(socketAddress, networkInterface);
-		} catch (Exception e) {
-			closeApplication(true);
-		}
-
-		receiveReader = new ReceiveReader();
-		receiveThread = new Thread(receiveReader);
-		receiveThread.start();
-		sender = new Sender(2);
-		sender.resume();
-	}
 
 	class ReceiveReader extends Processing {
 
@@ -97,4 +97,108 @@ public class ScreenApp extends BaseScreen {
 		}
 	}
 
+	public ScreenApp() {
+		initializeComponents();
+		addListeners();
+		setSizeLocation();
+		initializeSockets();
+	}
+
+	private void setSizeLocation() {
+		setSize(240, 480);
+		setLocationRelativeTo(null);
+	}
+
+	private void addListeners() {
+		addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (e.getX() >= 90 && e.getX() <= 146 && e.getY() >= 442 && e.getY() <= 466) {
+					closeApplication();
+				}
+			}
+		});
+	}
+
+	private void initializeComponents() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setUndecorated(true);
+		setVisible(true);
+
+		setShape(new RoundRectangle2D.Float(0, 0, 240, 480, 60, 60));
+		getContentPane().setLayout(null);
+
+		moverPanel = new JPanel();
+		moverPanel.setBounds(0, 0, 240, 45);
+		getContentPane().add(moverPanel);
+
+		new ComponentMover(this, moverPanel);
+
+		panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(9, 46, 222, 390);
+		getContentPane().add(panel);
+		panel.setLayout(new BorderLayout(5, 5));
+		
+		JPanel p = new JPanel();
+		JButton btn = new JButton("Button 1 (PAGE_START)");
+		p.add(btn);
+		panel.add(p, BorderLayout.PAGE_START);
+	}
+
+	private void initializeSockets() {
+		socketAddress = new InetSocketAddress("228.5.6.7", 4321);
+
+		try {
+			multicastSocket = new MulticastSocket(4321);
+			multicastSocket.setLoopbackMode(false);
+
+			InetAddress localHost = InetAddress.getLocalHost();
+			NetworkInterface networkInterface = NetworkInterface.getByInetAddress(localHost);
+			multicastSocket.joinGroup(socketAddress, networkInterface);
+		} catch (Exception e) {
+			closeApplication(true);
+		}
+
+		receiveReader = new ReceiveReader();
+		receiveThread = new Thread(receiveReader);
+		receiveThread.start();
+		sender = new Sender(2);
+		sender.resume();
+	}
+
+	protected final void closeApplication() {
+		closeApplication(false);
+	}
+
+	protected final void closeApplication(boolean force) {
+		String buttons[] = { "Sim", "Não" };
+		int result = !force ? showOptionDialog(null, "Deseja sair?", "Confirmação", DEFAULT_OPTION, WARNING_MESSAGE, null, buttons, buttons[1]) : YES_OPTION;
+		if (result == YES_OPTION) {
+			dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+		}
+	}
+
+	protected void printBackground(Graphics g) {
+		try {
+			g.drawImage(getImage(), 0, 0, null);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private BufferedImage image = null;
+
+	private Image getImage() throws IOException {
+		if (image == null) {
+			image = ImageIO.read(new File("images/smartphone.png"));
+		}
+		return image;
+	}
+
+	public void paint(Graphics g) {
+		super.paint(g);
+		printBackground(g);
+	}
 }
